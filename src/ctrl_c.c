2eee83313bcec3739d515586122d3513098b930a
Hamza Haddani <hhamza@student.1337.ma>
2022-06-02 10:02:16 +0100

    docs: Add doxygen docs for ctrl_c functions
    Add doxygen docs for:
    - hide_ctrl_c
    - show_ctrl_c

--- src/ctrl_c.c
+++ src/ctrl_c.c
@@ -6,12 +6,16 @@
 /*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 15:40:16 by hhamza            #+#    #+#             */
-/*   Updated: 2022/05/25 15:40:17 by hhamza           ###   ########.fr       */
+/*   Updated: 2022/06/02 09:59:24 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/**
+ * @brief Hide ^C when interruption signal (SIGINT) is sent
+ *
+ */
 void	hide_ctrl_c(void)
 {
 	struct termios	term;
@@ -21,7 +25,10 @@ void	hide_ctrl_c(void)
 	tcsetattr(STDIN_FILENO, 0, &term);
 }
 
-void	show_ctrl_c(void)
+/**
+ * @brief Restore ^C when interruption signal (SIGINT) is sent
+ *
+ */
 {
 	struct termios	term;
 
