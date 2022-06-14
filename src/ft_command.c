b097a70af7dadd07cbf4c51fded54f0de923ff3f
Hamza Haddani <hhamza@student.1337.ma>
2022-06-14 19:09:02 +0100

    fix(parser): Fix parenthesis errors & Add docs

--- src/ft_command.c
+++ src/ft_command.c
@@ -6,28 +6,53 @@
 /*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 13:05:08 by oaizab            #+#    #+#             */
-/*   Updated: 2022/06/14 15:19:37 by hhamza           ###   ########.fr       */
+/*   Updated: 2022/06/14 19:07:07 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/**
+ * @brief Test if token is a command (Command list or opening parenthesis)
+ *
+ * @param type: Token type.
+ * @return bool: True if token is a command, false otherwise.
+ */
 bool	ft_is_command(t_token_type type)
 {
 	return (ft_is_cmdlist(type) || type == TOKEN_OPAR);
 }
 
+/**
+ * @brief Test if token is a binary operator (&&, ||, |)
+ *
+ * @param type: Token type.
+ * @return bool: True if token is a binary operator, false otherwise.
+ */
 static bool	ft_is_binary(t_token_type type)
 {
 	return (type == TOKEN_OR || type == TOKEN_AND || type == TOKEN_PIPE \
 		|| type == TOKEN_END);
 }
 
+/**
+ * @brief Test if parenthesis expression is closed
+ *
+ * @param type: Current token type.
+ * @return bool: True if parenthesis expression is closed, false otherwise.
+ */
 static bool	ft_is_closed(t_token_type type)
 {
 	return (type == TOKEN_CPAR || ft_is_binary(type));
 }
 
+/**
+ * @brief Parse a subshell
+ *
+ * @param scanner: Scanner object.
+ * @param lvl: Parenthesis depth level.
+ * @return t_ast_node*: Subshell node, NULL on failure.
+ */
 static t_ast_node	*ft_parse_subshell(t_scanner *scanner, int *lvl)
 {
 	t_ast_node	*subshell;
@@ -36,9 +61,15 @@ static t_ast_node	*ft_parse_subshell(t_scanner *scanner, int *lvl)
 	if (ft_is_command(ft_scanner_peek(scanner)->type))
 		subshell = ft_parse_cmdline(scanner);
 	else
+	{
+		*lvl = 0;
 		return (ft_error(ERR_SYNTAX, ft_scanner_peek(scanner)), NULL);
+	}
 	if (subshell == NULL)
+	{
+		*lvl = 0;
 		return (NULL);
+	}
 	if (ft_scanner_peek(scanner)->type != TOKEN_CPAR)
 	{
 		*lvl = 0;
@@ -49,6 +80,12 @@ static t_ast_node	*ft_parse_subshell(t_scanner *scanner, int *lvl)
 	return (subshell);
 }
 
+/**
+ * @brief Parse a command
+ *
+ * @param scanner: Scanner object.
+ * @return t_ast_node*: Command node, NULL on failure.
+ */
 t_ast_node	*ft_parse_command(t_scanner *scanner)
 {
 	t_ast_node	*subshell;
