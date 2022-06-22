0223504d65fab1dfd06ce6e599f2c254928a3bc7
Hamza Haddani <hhamza@student.1337.ma>
2022-06-22 18:27:34 +0100

    refactor: Fix norm on ft_export_add

--- src/ft_export.c
+++ src/ft_export.c
@@ -3,14 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_export.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oaizab <oaizab@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 11:15:11 by oaizab            #+#    #+#             */
-/*   Updated: 2022/06/21 10:58:25 by oaizab           ###   ########.fr       */
+/*   Updated: 2022/06/22 18:11:58 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	ft_export_add_helper(t_env **env, t_env *new, char *key)
+{
+	t_env	*tmp;
+
+	if (!*env)
+		*env = new;
+	else if (strcmp(key, (*env)->key) < 0)
+	{
+		new->next = (*env);
+		*env = new;
+	}
+	else
+	{
+		tmp = *env;
+		while (tmp->next && ft_strcmp(tmp->next->key, key) < 0)
+			tmp = tmp->next;
+		new->next = tmp->next;
+		tmp->next = new;
+	}
+}
 
 void	ft_export_add(t_env **env, char *key, char *value)
 {
@@ -34,21 +55,7 @@ void	ft_export_add(t_env **env, char *key, char *value)
 		ft_env_clear(env);
 		return ;
 	}
-	if (!*env)
-		*env = new;
-	else if (strcmp(key, (*env)->key) < 0)
-	{
-		new->next = (*env);
-		*env = new;
-	}
-	else
-	{
-		tmp = *env;
-		while (tmp->next && ft_strcmp(tmp->next->key, key) < 0)
-			tmp = tmp->next;
-		new->next = tmp->next;
-		tmp->next = new;
-	}
+	ft_export_add_helper(env, new, key);
 }
 
 t_env	*ft_export_init(char **env)
