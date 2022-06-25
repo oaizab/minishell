8eed8748868a4bc556d2d1a7f4e3725ad3bb66c0
Hamza Haddani <hhamza@student.1337.ma>
2022-06-25 18:02:03 +0100

    feat(builtin): Add ft_echo

--- inc/ft_builtin.h
+++ inc/ft_builtin.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_builtin.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oaizab <oaizab@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 14:33:10 by oaizab            #+#    #+#             */
-/*   Updated: 2022/06/19 16:19:28 by oaizab           ###   ########.fr       */
+/*   Updated: 2022/06/25 17:13:07 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 # define FT_BUILTIN_H
 
 # include "minishell.h"
+
+int	ft_echo(char **args, int fd);
 
 #endif
--- src/ft_builtin_echo.c
+++ src/ft_builtin_echo.c
@@ -3,27 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   ft_builtin_echo.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oaizab <oaizab@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 14:33:40 by oaizab            #+#    #+#             */
-/*   Updated: 2022/06/19 16:21:36 by oaizab           ###   ########.fr       */
+/*   Updated: 2022/06/25 17:48:47 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int	ft_count_args(char **args)
+{
+	int	i;
+
+	i = 0;
+	while (args[i] != NULL)
+	{
+		i++;
+	}
+	return (i);
+}
+
 int	ft_echo(char **args, int fd)
 {
+	int		argc;
 	char	end;
 	int		i;
 
-	i = 1;
+	argc = ft_count_args(args);
+	if (argc == 1)
+	{
+		return (ft_putchar_fd('\n', fd), 0);
+	}
 	end = '\n';
+	i = 1;
 	if (ft_strcmp(args[1], "-n") == 0)
+	{
 		end = '\0';
-	while (args[i])
+		++i;
+	}
+	while (args[i] != NULL)
 	{
 		ft_putstr_fd(args[i], fd);
+		if (i != argc - 1)
+			ft_putchar_fd(' ', fd);
 		i++;
 	}
 	ft_putchar_fd(end, fd);
