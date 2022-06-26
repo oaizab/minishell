1ac00c83a9763faf3a059a49294d26256f4fa704
Hamza Haddani <hhamza@student.1337.ma>
2022-06-26 20:12:34 +0100

    feat(builtin): Add ft_pwd

--- src/ft_builtin_pwd.c
+++ src/ft_builtin_pwd.c
@@ -1,24 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_builtin.h                                       :+:      :+:    :+:   */
+/*   ft_builtin_pwd.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/19 14:33:10 by oaizab            #+#    #+#             */
-/*   Updated: 2022/06/26 09:37:43 by hhamza           ###   ########.fr       */
+/*   Created: 2022/06/26 09:24:38 by hhamza            #+#    #+#             */
+/*   Updated: 2022/06/26 12:00:02 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_BUILTIN_H
-# define FT_BUILTIN_H
+#include "minishell.h"
 
-# include "minishell.h"
+extern int errno;
 
-int	ft_echo(char **args, int fd);
-int	ft_cd(char **args, t_env **env);
-int	ft_pwd(int fd);
+/**
+ * @brief Print current working directory.
+ *
+ * @return int: 0 if success, 1 on failure
+ */
+int	ft_pwd(int fd)
+{
+	char	*cwd;
 
-int	ft_count_args(char **args);
-
-#endif
+	cwd = getcwd(NULL, 0);
+	if (cwd == NULL)
+	{
+		ft_fprintf(STDERR_FILENO, "minishell: pwd: %s\n", strerror(errno));
+		return (1);
+	}
+	ft_fprintf(fd, "%s\n", cwd);
+	free(cwd);
+	return (0);
+}
