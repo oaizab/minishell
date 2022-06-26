8d7001f063f9bd2ec23c98e9f6ee5aac5f729de7
Omar Aizab <omaraizab98@gmail.com>
2022-06-26 10:58:43 +0100

    feat(prompt): add colored prompt.

--- src/ft_read_cmd.c
+++ src/ft_read_cmd.c
@@ -3,14 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_read_cmd.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: oaizab <oaizab@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 09:29:57 by hhamza            #+#    #+#             */
-/*   Updated: 2022/06/15 09:29:58 by hhamza           ###   ########.fr       */
+/*   Updated: 2022/06/25 18:24:37 by oaizab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static char	*ft_get_prompt(void)
+{
+	char	*prompt;
+	char	*path;
+	char	*basename;
+
+	prompt = ft_strdup("\001\e[01;32m\002");
+	path = getcwd(NULL, 0);
+	basename = ft_strrchr(path, '/');
+	if (basename)
+		basename++;
+	else
+		basename = path;
+	basename = ft_strdup(basename);
+	free(path);
+	prompt = ft_append_str(prompt, basename);
+	free(basename);
+	prompt = ft_append_str(prompt, "\001\e[00m\002 $ ");
+	return (prompt);
+}
 
 /**
  * @brief Read command from standard input
@@ -21,8 +42,11 @@
 char	*ft_read_cmd(void)
 {
 	char	*cmd;
+	char	*prompt;
 
-	cmd = readline("minishell $ ");
+	prompt = ft_get_prompt();
+	cmd = readline(prompt);
+	free(prompt);
 	if (cmd == NULL)
 	{
 		ft_exit(EXIT_SUCCESS);
