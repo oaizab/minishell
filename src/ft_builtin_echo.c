079aa0e290951d8fc25eb63ce0b697e62580ba5c
Hamza Haddani <hhamza@student.1337.ma>
2022-06-26 20:04:15 +0100

    refactor(builtin): Move ft_count_args
    Move ft_count_args to its own separate file

--- src/ft_builtin_echo.c
+++ src/ft_builtin_echo.c
@@ -6,29 +6,11 @@
 /*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 14:33:40 by oaizab            #+#    #+#             */
-/*   Updated: 2022/06/26 07:14:56 by hhamza           ###   ########.fr       */
+/*   Updated: 2022/06/26 07:38:43 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-/**
- * @brief Count the number of strings in NULL terminated string array.
- *
- * @param args: NULL terminated string array
- * @return int: Number of strings
- */
-static int	ft_count_args(char **args)
-{
-	int	i;
-
-	i = 0;
-	while (args[i] != NULL)
-	{
-		i++;
-	}
-	return (i);
-}
 
 /**
  * @brief Output arguments separated by space, terminated with a newline. In -n
--- src/ft_builtin_utils.c
+++ src/ft_builtin_utils.c
@@ -1,22 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_builtin.h                                       :+:      :+:    :+:   */
+/*   ft_builtin_utils.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/19 14:33:10 by oaizab            #+#    #+#             */
-/*   Updated: 2022/06/25 17:13:07 by hhamza           ###   ########.fr       */
+/*   Created: 2022/06/26 07:38:28 by hhamza            #+#    #+#             */
+/*   Updated: 2022/06/26 07:38:52 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_BUILTIN_H
-# define FT_BUILTIN_H
+#include "minishell.h"
 
-# include "minishell.h"
+/**
+ * @brief Count the number of strings in NULL terminated string array.
+ *
+ * @param args: NULL terminated string array
+ * @return int: Number of strings
+ */
+int	ft_count_args(char **args)
+{
+	int	i;
 
-int	ft_echo(char **args, int fd);
-
-int	ft_count_args(char **args);
-
-#endif
+	i = 0;
+	while (args[i] != NULL)
+	{
+		i++;
+	}
+	return (i);
+}
