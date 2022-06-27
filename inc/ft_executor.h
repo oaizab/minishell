38514327eeaee1795eab745e4892e3cdd84cbcd1
Hamza Haddani <hhamza@student.1337.ma>
2022-06-27 20:24:01 +0100

    feat(executor): Add ft_execute_builtin

--- inc/ft_executor.h
+++ inc/ft_executor.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_executor.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oaizab <oaizab@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 13:44:00 by oaizab            #+#    #+#             */
-/*   Updated: 2022/06/27 13:29:46 by oaizab           ###   ########.fr       */
+/*   Updated: 2022/06/27 19:25:29 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,8 @@
 void	ft_executor(t_ast_node *root, t_ft_env *env);
 bool	ft_execute_redir(t_ast_node *node);
 void	ft_execute_cmd(t_ast_node *node, t_env *env);
+
+bool	ft_is_builtin(char *cmd);
+void	ft_execute_builtin(t_ast_node *root, t_ft_env *ft_env);
 
 #endif
---
+++ src/ft_execute_builtin.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_execute_builtin.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/27 19:16:37 by hhamza            #+#    #+#             */
+/*   Updated: 2022/06/27 19:27:04 by hhamza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+bool	ft_is_builtin(char *cmd)
+{
+	return (ft_strcmp(cmd, "echo") == 0 \
+		|| ft_strcmp(cmd, "cd") == 0 \
+		|| ft_strcmp(cmd, "pwd") == 0 \
+		|| ft_strcmp(cmd, "export") == 0 \
+		|| ft_strcmp(cmd, "unset") == 0 \
+		|| ft_strcmp(cmd, "env") == 0 \
+		|| ft_strcmp(cmd, "exit") == 0);
+}
+
+void	ft_execute_builtin(t_ast_node *root, t_ft_env *ft_env)
+{
+	if (ft_is_builtin(root->value) == false)
+	{
+		return ;
+	}
+	if (ft_strcmp(root->value, "echo") == 0)
+		g_exit_status = ft_echo(root->args, STDOUT_FILENO);
+	else if (ft_strcmp(root->value, "cd") == 0)
+		g_exit_status = ft_cd(root->args, &ft_env->env);
+	else if (ft_strcmp(root->value, "pwd") == 0)
+		g_exit_status = ft_pwd(STDOUT_FILENO);
+	else if (ft_strcmp(root->value, "export") == 0)
+		ft_export(root->args, ft_env);
+}
