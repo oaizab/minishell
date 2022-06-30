3cd09d97b12ce836a984bee7f63d8754997b2c97
Hamza Haddani <hhamza@student.1337.ma>
2022-06-30 17:07:26 +0100

    fix(builtin): Fix exit on too many arguments

--- src/ft_builtin_exit.c
+++ src/ft_builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 10:04:16 by hhamza            #+#    #+#             */
-/*   Updated: 2022/06/30 07:12:47 by hhamza           ###   ########.fr       */
+/*   Updated: 2022/06/30 17:00:18 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,17 @@ void	ft_exit(char **args, t_ft_env *ft_env)
 	int	argc;
 	int	status;
 
+	argc = ft_count_args(args);
+	ft_printf("exit\n");
+	if (argc != 2)
+	{
+		ft_fprintf(STDERR_FILENO, "minishell: exit: too many arguments\n");
+		return ;
+	}
 	ft_env_clear(&ft_env->env);
 	ft_env_clear(&ft_env->export);
 	ft_restore_ctrl_c();
 	rl_clear_history();
-	ft_printf("exit\n");
-	argc = ft_count_args(args);
 	if (argc == 1)
 	{
 		exit(g_exit_status);
